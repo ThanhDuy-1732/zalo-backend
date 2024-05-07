e0565be2b52483eccb8d47edee8726fb842b860b
DuyHa <duy.ha@ahasoft.vn>
2024-05-07 08:53:04 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://h5.zdn.vn', 'zbrowser://h5.zdn.vn'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 10000);
 }
 bootstrap();
